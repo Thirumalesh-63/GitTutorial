231e4a1d14d007a372bb7c76eaf749dbca0344cd
Thirumalesh-63 <gtm5562@gmail.com>
2025-02-05 14:34:56 +0530

    first commi2t

---
+++ src/test/java/com/zapcom/GitTutorial/GitTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapcom.GitTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
